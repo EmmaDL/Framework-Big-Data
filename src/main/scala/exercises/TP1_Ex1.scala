7b4519e553e1d6c9954e87f55e39093bb02b4921
Emma Dollé <emma.dolle-leczycki@esme.fr>
2020-11-23 12:27:14 +0100

    TP1 with ex1q5

--- src/main/scala/exercises/TP1_Ex1.scala
+++ src/main/scala/exercises/TP1_Ex1.scala
@@ -1,6 +1,7 @@
 package exercises
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object TP1_Ex1 {
@@ -30,10 +31,25 @@ object TP1_Ex1 {
     val total_vues_ldc = rdd_DiCaprio.map(item => (item.split(";")(1).toDouble))
     val total_vues = rdd.map(item => (item.split(";")(1).toDouble))
     val pourcentage_vues_ldc = (total_vues_ldc.sum() / total_vues.sum())*100
-    print("Le pourcentage des vues des films de Leonardo Di Caprio est de " +pourcentage_vues_ldc + "%")
+    print("Le pourcentage des vues des films de Leonardo Di Caprio est de " +pourcentage_vues_ldc + "%\n")
 
 
     //Question 5
+    //Moyenne des notes par acteur
+    val count: RDD[(String, Double)] = rdd.map(item => ((item.split(";")(3)), (item.split(";")(2).toDouble)))
+    val withValue = count.mapValues(e => (1.0, e))
+    val countSums = withValue.reduceByKey((x,y) => (x._1 + y._1, x._2 + y._2 ))
+    val keyMeans = countSums.mapValues(avgCount => avgCount._2 / avgCount._1)
+    print("Moyenne des notes par acteur:\n")
+    keyMeans.foreach(println)
+
+    //Moyenne des vues par acteur
+    val count2: RDD[(String, Double)] = rdd.map(item => ((item.split(";")(3)), (item.split(";")(1).toDouble)))
+    val withValue2 = count2.mapValues(e => (1.0, e))
+    val countSums2 = withValue2.reduceByKey((x,y) => (x._1 + y._1, x._2 + y._2 ))
+    val keyMeans2 = countSums2.mapValues(avgCount => avgCount._2 / avgCount._1)
+    print("Moyenne des vues par acteur:\n")
+    keyMeans2.foreach(println)
 
   }
 
